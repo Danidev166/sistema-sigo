f1530f334a29373b7e8fc4730cd79f8e9d0b77a8
danidev166 <sigosistemaintegraldeorientaci@gmail.com>
2025-10-08 00:26:44 -0300

    Fix psicosocial cache issue - disable cache for fresh data

--- sigo-frontend/src/features/seguimiento-psicosocial/services/seguimientoPsicosocialService.js
+++ sigo-frontend/src/features/seguimiento-psicosocial/services/seguimientoPsicosocialService.js
@@ -1,7 +1,9 @@
 import api from "../../../services/axios";
 
 const seguimientoPsicosocialService = {
-  getSeguimientos: () => api.get("/seguimiento-psicosocial"),
+  getSeguimientos: () => api.get("/seguimiento-psicosocial", { 
+    headers: { "Cache-Control": "no-cache" } 
+  }),
   getSeguimientoById: (id) => api.get(`/seguimiento-psicosocial/${id}`),
   crearSeguimiento: (data) => api.post("/seguimiento-psicosocial", data),
   actualizarSeguimiento: (id, data) => api.put(`/seguimiento-psicosocial/${id}`, data),
